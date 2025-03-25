35ba96294754dc35167ad282c2b7a582b572acf5
QAzando <qazando@gmail.com>
2025-03-24 21:46:48 -0300

    Subindo novos testes

--- playwright.config.js
+++ playwright.config.js
@@ -16,10 +16,11 @@ export default defineConfig({
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
+  timeout: 30 * 1000, // 30 segundos
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: 2,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
--- tests/LoginTest.spec.js
+++ tests/LoginTest.spec.js
@@ -11,16 +11,14 @@ test('Login com sucesso', async ({ page }) => {
 
 test('Login com email vazio', async ({ page }) => {
   await page.goto('https://automationpratice.com.br/login');
-  await page.locator('#user').fill('herrbert@gmail.com');
   await page.locator('#password').fill('123456');
   await page.getByRole('button', { name: 'login' }).click();
-  await expect(page.getByRole('heading', { name: 'Login realizado' })).toBeVisible();
+  await expect(page.getByText('E-mail inválido.')).toBeVisible();
 });
 
 test('Login com senha vazia', async ({ page }) => {
   await page.goto('https://automationpratice.com.br/login');
   await page.locator('#user').fill('herrbert@gmail.com');
-  await page.locator('#password').fill('123456');
   await page.getByRole('button', { name: 'login' }).click();
-  await expect(page.getByRole('heading', { name: 'Login realizado' })).toBeVisible();
+  await expect(page.getByText('Senha inválida.')).toBeVisible();
 });
